cf8061e40bbdae22787c71bf2dd7d56dff088fbf
aniketvs <sharmavinod8454@gmail.com>
2025-06-17 00:56:57 +0530

    auth done

--- jobs/tokenDelete.cron.js
+++ jobs/tokenDelete.cron.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/config');
 cron.schedule('0 * * * *', async () => { 
     try {
        const query = 'DELETE FROM user_session WHERE expires_at < NOW();';
-       const [results] = await sequelize.query(query);
+      //  const [results] = await sequelize.query(query);
        console.log(`Deleted expired sessions: ${results.affectedRows}`);
     } catch (error) {
        console.error('Error deleting expired sessions:', error);
